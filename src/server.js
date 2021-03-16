2944be5da049fea57f9ed1aa9fdc91763661d15f
alexanderescobar <alexander.escobar@carvajal.com>
2021-03-16 16:04:29 -0500

    Modification of server-js to call main page

--- src/server.js
+++ src/server.js
@@ -3,9 +3,14 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/../../dist/angular-movies-search'));
+/*app.use(express.static(__dirname+'/../../dist/angular-movies-search'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/../../dist/angular-movies-search/index.html'));
-});
+});*/
+
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
 
 app.listen(process.env.PORT || 8080);
