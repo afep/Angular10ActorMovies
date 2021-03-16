9c964da90a1c947ef1970043f9714496391be2de
alexanderescobar <alexander.escobar@carvajal.com>
2021-03-16 15:51:05 -0500

    Adding styles to seach page

--- src/app/services/actor-movies-service.ts
+++ src/app/services/actor-movies-service.ts
@@ -9,7 +9,7 @@ export class ActorMoviesService {
 
     getMoviesByCriteria(query) {
         //return this.http.get(`https://www.omdbapi.com/?apikey=d3f6c0ee&s=${query}`);
-        return this.http.get(`https://api.themoviedb.org/3/search/person?api_key=07ceecc88ea4a1c377aa1a1950e0e784&language=en-US&query=${query}&page=1&include_adult=false`);
+        return this.http.get(`https://api.themoviedb.org/3/search/person?api_key=07ceecc88ea4a1c377aa1a1950e0e784&language=en-US&query=${query}&page=1&include_adult=false&certification.lte=PG-13,PG,NR`);
     }
 
 }
