2b22d4fa2223d0e5adc654c0c19d55bf6247f07a
alexanderescobar <alexander.escobar@carvajal.com>
2021-03-16 15:10:29 -0500

    Changing path on server.js

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/angular-movies-search'));
+app.use(express.static(__dirname+'/../../dist/angular-movies-search'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/angular-movies-search/index.html'));
+    res.sendFile(path.join(__dirname+'/../../dist/angular-movies-search/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
