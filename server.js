74065618cb616bee3a8632e0dca67ebeddb777ac
alexanderescobar <alexander.escobar@carvajal.com>
2021-03-23 08:52:20 -0500

    Heroku port configuration trial

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 const express = require('path');
 
 app.use(express.static(__dirname+'/dist'));
-
-app.listen(process.env.PORT || 8080);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT);
 
 //PathLocationStrategy
 app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
-
+app.set("port", PORT);
 console.log('Console working!');
